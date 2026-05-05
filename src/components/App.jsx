fa9fa08ab4103694074c116d874c0a343767ba91
Sonia <sonia.wangari@student.moringaschool.com>
2026-05-05 21:20:12 +0300

    Blog site created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,7 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+     Happy coding! I would like to show you the wonders of coding including both the advantages and disadvantages of coding";
     </div>
   );
 }
